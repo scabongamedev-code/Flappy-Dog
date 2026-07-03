e2f18bc77587c497108dbac9bd74f84c9d6ed6eb
scabongamedev-code <scabon.game.dev@gmail.com>
2026-07-03 02:47:34 -0400

    add sound effects

--- scripts/death_area.gd
+++ scripts/death_area.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
-
+@onready var die_sound: AudioStreamPlayer = $"die sound"
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		print("Player Enetered Death Area")
+		die_sound.play()
+		await die_sound.finished
 		get_tree().quit()
--- scripts/pipe.gd
+++ scripts/pipe.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @export var pipe_speed : float = 100.0
 
+@onready var point_sound: AudioStreamPlayer = $"point sound"
+
 func _process(delta: float) -> void:
 	position += Vector2.LEFT * pipe_speed * delta
 	
@@ -12,4 +14,5 @@ func _process(delta: float) -> void:
 
 func _on_score_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
+		point_sound.play()
 		Global.score += 1
